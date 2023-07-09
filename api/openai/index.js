559c2e187086810e003310fd648b166c4156b613
Sinthorn <psinthorn@gmail.com>
2023-07-09 18:59:49 +0700

    add react frontend and rearrange folder structure for scalable

--- api/openai/index.js
+++ api/openai/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv').config();
 
 // declair express constant
@@ -6,6 +7,9 @@ const app = express();
 // enable json body parser 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// create static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // declair server port 
 const srvPort = process.env.PORT || 4000;
