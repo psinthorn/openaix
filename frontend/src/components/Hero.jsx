5a4db13fa10d785fc53bd825d8d10b08b0912c20
Sinthorn <psinthorn@gmail.com>
2023-07-10 00:56:37 +0700

    work with tailwindcss on user interface

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,25 @@
+// import logo from '../assets';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleRight, faCheckSquare,faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+const Hero = () => {
+  return (
+    <header className='flex flex-col  w-full justify-center items-center'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+      <label className='text-xl font-bold orange_gradient'><FontAwesomeIcon icon={faCheckSquare} /> OpenAI<span class="text-gray-600">x*</span></label>
+        <button type="button" className='black_btn'>
+          Login
+        </button>
+      </nav>
+      <h1 className='head_text'>Generate your post and image Banner with
+        <br className='max-md:hidden'/>
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Draft your post and image banner in one click with AI Power then re-arrage it to your own styles let's  get start now<FontAwesomeIcon className='pl-1' icon={faAngleRight} />
+      </h2>
+    </header>
+  )
+}
+
+export default Hero
