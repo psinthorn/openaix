69875f4a2a0fcd92924deacfcc60cedff877ad4d
Sinthorn <psinthorn@gmail.com>
2025-02-01 09:35:50 +0700

    update hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -7,12 +7,12 @@ const Hero = () => {
   return (
     <header className='flex flex-col  w-full justify-center items-center'>
       <Navbar />
-      <h1 className='head_text'>Generate your post and image Banner with
+      <h1 className='head_text'>Powering the Future with AI, IoT & Cloud Innovation
         <br className='max-md:hidden'/>
-        <span className="orange_gradient">OpenAI GPT-4</span>
+        <span className="orange_gradient">Innovate. Automate. Elevate.</span>
       </h1>
       <h2 className='desc'>
-        Draft your post and image banner in one click with AI Power then re-arrage it to your own styles let's  get start now<FontAwesomeIcon className='pl-1' icon={faAngleRight} />
+      Mission to empower businesses through smart technologies. Start Now<FontAwesomeIcon className='pl-1' icon={faAngleRight} />
       </h2>
     </header>
   )
